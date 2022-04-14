9a1395862ae3d6c6ddaa427263754f1ed4b56ecc
deowork <deoworkpr@gmail.com>
2022-04-14 22:58:50 +0300

    App Navbar; User Card

--- src/components/Layouts/AppLayout.tsx
+++ src/components/Layouts/AppLayout.tsx
@@ -1,24 +1,140 @@
-import Navigation from '@/components/Layouts/Navigation'
+import React from 'react'
 import { useAuth } from '@/hooks/auth'
+import {
+  AppShell,
+  Navbar,
+  Group,
+  ScrollArea,
+  createStyles,
+} from '@mantine/core'
+import {
+  Users,
+  AddressBook,
+  Gauge,
+  PresentationAnalytics,
+  FileAnalytics,
+  Adjustments,
+  Lock,
+} from 'tabler-icons-react'
+import { UserCard } from '@/components/UserCard/UserCard'
+import { LinksGroup } from '@/components/LinksGroup/LinksGroup'
+import ApplicationLogo from '@/components/ApplicationLogo'
+
+const navbarLinks = [
+  { label: 'Dashboard', icon: Gauge },
+  {
+    label: 'Clients',
+    icon: AddressBook,
+    initiallyOpened: true,
+    links: [
+      { label: 'Manage', link: '/' },
+      { label: 'Create', link: '/' },
+    ],
+  },
+  { label: 'Contracts', icon: FileAnalytics },
+  {
+    label: 'Users',
+    icon: Users,
+    links: [
+      { label: 'Manage', link: '/' },
+      { label: 'Create', link: '/' },
+      { label: 'Roles', link: '/' },
+    ],
+  },
+  { label: 'Analytics', icon: PresentationAnalytics },
+  { label: 'Settings', icon: Adjustments },
+  {
+    label: 'Security',
+    icon: Lock,
+    links: [
+      { label: 'Enable 2FA', link: '/' },
+      { label: 'Change password', link: '/' },
+      { label: 'Recovery codes', link: '/' },
+    ],
+  },
+]
+
+const useStyles = createStyles(theme => ({
+  navbar: {
+    backgroundColor:
+      theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.white,
+    paddingBottom: 0,
+  },
+
+  header: {
+    padding: theme.spacing.md,
+    paddingTop: 0,
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    borderBottom: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+
+  links: {
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+  },
+
+  linksInner: {
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+  },
+
+  footer: {
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+    borderTop: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+}))
 
 const AppLayout = ({ header, children }) => {
-    const { user } = useAuth({ middleware: 'auth' })
+  const { user } = useAuth({ middleware: 'auth' })
 
-    return (
-        <div className="min-h-screen bg-gray-100">
-            <Navigation user={user} />
+  const { classes } = useStyles()
+  const links = navbarLinks.map(item => (
+    <LinksGroup {...item} key={item.label} />
+  ))
 
-            {/* Page Heading */}
-            <header className="bg-white shadow">
-                <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {header}
-                </div>
-            </header>
+  return (
+    <AppShell
+      padding="md"
+      navbar={
+        <Navbar
+          height={800}
+          width={{ sm: 300 }}
+          p="md"
+          className={classes.navbar}>
+          <Navbar.Section className={classes.header}>
+            <Group position="apart">
+              <ApplicationLogo width={60} fill="#ef3b2d" />
+            </Group>
+          </Navbar.Section>
 
-            {/* Page Content */}
-            <main>{children}</main>
-        </div>
-    )
+          <Navbar.Section grow className={classes.links} component={ScrollArea}>
+            <div className={classes.linksInner}>{links}</div>
+          </Navbar.Section>
+
+          <Navbar.Section className={classes.footer}>
+            <UserCard image={null} name={user?.name} email={user?.email} />
+          </Navbar.Section>
+        </Navbar>
+      }
+      styles={theme => ({
+        main: {
+          backgroundColor:
+            theme.colorScheme === 'dark'
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      })}>
+      {header}
+      {children}
+    </AppShell>
+  )
 }
 
 export default AppLayout
