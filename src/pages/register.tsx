bfeb96bc8da23f2c381ba4363868d20e87ebc64d
Kiv-i <kiv.italievich@gmail.com>
2022-04-15 10:12:45 +0300

    Register Page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,122 +1,187 @@
-import ApplicationLogo from '@/components/ApplicationLogo'
-import AuthCard from '@/components/AuthCard'
-import AuthValidationErrors from '@/components/AuthValidationErrors'
-import Button from '@/components/Button'
-import GuestLayout from '@/components/Layouts/GuestLayout'
-import Input from '@/components/Input'
-import Label from '@/components/Label'
-import Link from 'next/link'
-import { useAuth } from '@/hooks/auth'
-import { useState } from 'react'
+import {useForm} from '@mantine/form'
+import {
+  PasswordInput,
+  Group,
+  Button,
+  Box,
+  Center,
+  Paper,
+  Text,
+  Divider,
+  PaperProps,
+  TextInput,
+  Checkbox, Anchor
+} from '@mantine/core'
+import AuthCard from "@/components/AuthCard"
+import Link from "next/link"
+import ApplicationLogo from "@/components/ApplicationLogo"
+import {GithubButton, GoogleButton} from "@/components/SocialButtons/SocialButtons"
+import AuthValidationErrors from "@/components/AuthValidationErrors"
+import GuestLayout from "@/components/Layouts/GuestLayout"
+import {useState} from "react"
+import {useAuth} from "@/hooks/auth"
+import {At} from "tabler-icons-react"
+import {createStyles} from '@mantine/core';
 
-const Register = () => {
-    const { register } = useAuth({
-        middleware: 'guest',
-        redirectIfAuthenticated: '/dashboard',
-    })
+const useStyles = createStyles((theme, _params, getRef) => ({
+  link: {
+    color: '#adb5bd',
+    fontSize: '12px',
+    textAlign: 'left',
+    textDecoration: 'none'
+  }
+}))
 
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [password_confirmation, setPasswordConfirmation] = useState('')
-    const [errors, setErrors] = useState([])
+interface FormValues {
+  name: string
+  email: string
+  password: string
+  password_confirmation: string
+  remember: boolean
+}
 
-    const submitForm = event => {
-        event.preventDefault()
 
-        register({ name, email, password, password_confirmation, setErrors })
-    }
+const Register = (props: PaperProps<'div'>) => {
+  const {classes} = useStyles();
 
-    return (
-        <GuestLayout>
-            <AuthCard
-                logo={
-                    <Link href="/">
-                        <a>
-                            <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                        </a>
-                    </Link>
-                }>
+  const form = useForm({
+    initialValues: {
+      name: '',
+      email: '',
+      password: 'secret',
+      password_confirmation: 'sevret',
+      remember: false
+    },
 
-                {/* Validation Errors */}
-                <AuthValidationErrors className="mb-4" errors={errors} />
+    validate: {
+      password_confirmation: (value, values) =>
+        value !== values.password ? 'Passwords did not match' : null,
+    },
+  });
 
-                <form onSubmit={submitForm}>
-                    {/* Name */}
-                    <div>
-                        <Label htmlFor="name">Name</Label>
+  const {register} = useAuth({
+    middleware: 'guest',
+    redirectIfAuthenticated: '/dashboard',
+  })
 
-                        <Input
-                            id="name"
-                            type="text"
-                            value={name}
-                            className="block mt-1 w-full"
-                            onChange={event => setName(event.target.value)}
-                            required
-                            autoFocus
-                        />
-                    </div>
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [password_confirmation, setPasswordConfirmation] = useState('')
+  const [errors, setErrors] = useState([])
 
-                    {/* Email Address */}
-                    <div className="mt-4">
-                        <Label htmlFor="email">Email</Label>
+  const submitForm = async (props: FormValues) => {
+    await register({setErrors, ...props})
+  }
 
-                        <Input
-                            id="email"
-                            type="email"
-                            value={email}
-                            className="block mt-1 w-full"
-                            onChange={event => setEmail(event.target.value)}
-                            required
-                        />
-                    </div>
+  return (
+    <GuestLayout>
+      <AuthCard
+        logo={
+          <Center mt={30}>
+            <Link href="/">
+              <a>
+                <ApplicationLogo width="80" height="80" fill="#ef3b2d"/>
+              </a>
+            </Link>
+          </Center>
+        }>
+        <Paper
+          radius="md"
+          p="xl"
+          sx={{maxWidth: 500}}
+          mx="auto"
+          mt={30}
+          withBorder
+          {...props}>
+          <Text size="lg" weight={500}>
+            Welcome to NextCRM
+          </Text>
 
-                    {/* Password */}
-                    <div className="mt-4">
-                        <Label htmlFor="password">Password</Label>
+          <Group grow mb="md" mt="md">
+            <GoogleButton radius="xl">Google</GoogleButton>
+            <GithubButton radius="xl">Github</GithubButton>
+          </Group>
 
-                        <Input
-                            id="password"
-                            type="password"
-                            value={password}
-                            className="block mt-1 w-full"
-                            onChange={event => setPassword(event.target.value)}
-                            required
-                            autoComplete="new-password"
-                        />
-                    </div>
+          <Divider
+            label="Or continue with email"
+            labelPosition="center"
+            my="lg"
+          />
 
-                    {/* Confirm Password */}
-                    <div className="mt-4">
-                        <Label htmlFor="password_confirmation">
-                            Confirm Password
-                        </Label>
+          <AuthValidationErrors mb={15} errors={errors}/>
 
-                        <Input
-                            id="password_confirmation"
-                            type="password"
-                            value={password_confirmation}
-                            className="block mt-1 w-full"
-                            onChange={event =>
-                                setPasswordConfirmation(event.target.value)
-                            }
-                            required
-                        />
-                    </div>
+          <form onSubmit={form.onSubmit(submitForm)}>
 
-                    <div className="flex items-center justify-end mt-4">
-                        <Link href="/login">
-                            <a className="underline text-sm text-gray-600 hover:text-gray-900">
-                                Already registered?
-                            </a>
-                        </Link>
+            <Group direction="column" grow>
 
-                        <Button className="ml-4">Register</Button>
-                    </div>
-                </form>
-            </AuthCard>
-        </GuestLayout>
-    )
+              <TextInput label="Name" placeholder="Name" {...form.getInputProps('name')} />
+
+              <TextInput
+                required
+                autoFocus
+                icon={<At/>}
+                label="Email"
+                id="email"
+                type="email"
+                placeholder="your@email.com"
+                value={form.values.email}
+                onChange={event =>
+                  form.setFieldValue('email', event.currentTarget.value)
+                }
+                error={form.errors.email && 'Invalid email'}
+                {...form.getInputProps('email')}
+              />
+
+              <PasswordInput
+                id="password"
+                type="password"
+                value={password}
+                label="Password"
+                placeholder="Password"
+                onChange={event => setPassword(event.target.value)}
+                required
+                autoComplete="new-password"
+                {...form.getInputProps('password')}
+              />
+
+              <PasswordInput
+                required
+                id="password_confirmation"
+                mt="sm"
+                label="Confirm password"
+                placeholder="Confirm password"
+                type="password"
+                value={password_confirmation}
+                onChange={event =>
+                  setPasswordConfirmation(event.target.value)
+                }
+                {...form.getInputProps('password_confirmation')}
+              />
+
+              <Checkbox
+                mt="md"
+                id="remember_me"
+                name="remember"
+                label="Remember me"
+                {...form.getInputProps('remember', {type: 'checkbox'})}
+              />
+
+            </Group>
+
+            <Group position="apart" mt="xl">
+              <Anchor component={Link} href="/login">
+                <a className={classes.link}>
+                  Already registered?
+                </a>
+              </Anchor>
+              <Button type="submit">Submit</Button>
+            </Group>
+          </form>
+        </Paper>
+      </AuthCard>
+    </GuestLayout>
+  );
 }
 
 export default Register
