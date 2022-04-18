2eacd8a985612a40d85f29af462ddefe5169cb7d
deowork <deoworkpr@gmail.com>
2022-04-18 08:58:32 +0300

    404 page; UI dots component

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,125 @@
-const NotFoundPage = () => (
-    <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        <div className="max-w-xl mx-auto sm:px-6 lg:px-8">
-            <div className="flex items-center pt-8 sm:justify-start sm:pt-0">
-                <div className="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
-                    404
-                </div>
+import React from 'react'
+import Link from 'next/link'
+import { Dots } from '@/components/UI/Dots'
+import {
+  createStyles,
+  Title,
+  Text,
+  Button,
+  Container,
+  Group,
+} from '@mantine/core'
+import { ChevronLeft, ChevronRight } from 'tabler-icons-react'
+import { useAuth } from '@/hooks/auth'
 
-                <div className="ml-4 text-lg text-gray-500 uppercase tracking-wider">
-                    Not Found
-                </div>
-            </div>
-        </div>
-    </div>
-)
+const useStyles = createStyles(theme => ({
+  root: {
+    paddingTop: 80,
+    paddingBottom: 80,
+  },
+
+  dots: {
+    position: 'absolute',
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[5]
+        : theme.colors.gray[1],
+
+    '@media (max-width: 755px)': {
+      display: 'none',
+    },
+  },
+
+  label: {
+    textAlign: 'center',
+    fontWeight: 700,
+    fontSize: 180,
+    lineHeight: 1,
+    marginBottom: theme.spacing.xl * 1.8,
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[4]
+        : theme.colors.gray[2],
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 120,
+    },
+  },
+
+  title: {
+    textAlign: 'center',
+    fontWeight: 900,
+    fontSize: 38,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 32,
+    },
+  },
+
+  description: {
+    maxWidth: 500,
+    margin: 'auto',
+    marginTop: theme.spacing.xl,
+    marginBottom: theme.spacing.xl * 1.5,
+  },
+}))
+
+const NotFoundPage = () => {
+  const { classes } = useStyles()
+  const { user } = useAuth()
+
+  return (
+    <Container className={classes.root}>
+      <Dots className={classes.dots} style={{ left: 0, top: 0 }} />
+      <Dots className={classes.dots} style={{ left: 60, top: 0 }} />
+      <Dots className={classes.dots} style={{ left: 0, top: 140 }} />
+      <Dots className={classes.dots} style={{ right: 0, top: 40 }} />
+
+      <Text className={classes.label}>404</Text>
+      <Title className={classes.title}>
+        You have found a{' '}
+        <Text
+          inherit
+          component="span"
+          variant="gradient"
+          gradient={{ from: 'blue', to: 'cyan' }}>
+          secret place
+        </Text>
+      </Title>
+      <Text
+        color="dimmed"
+        size="lg"
+        align="center"
+        className={classes.description}>
+        Unfortunately, this is only a 404 page. You may have mistyped the
+        address, or the page has been moved to another URL
+      </Text>
+      <Group position="center">
+        <Link href="/" passHref>
+          <Button
+            leftIcon={<ChevronLeft size={14} />}
+            component="a"
+            variant="subtle"
+            size="md">
+            Take me back to home page
+          </Button>
+        </Link>
+        {user ? (
+          <Link href="/dashboard" passHref>
+            <Button
+              rightIcon={<ChevronRight size={14} />}
+              component="a"
+              variant="subtle"
+              size="md">
+              Dashboard
+            </Button>
+          </Link>
+        ) : (
+          <></>
+        )}
+      </Group>
+    </Container>
+  )
+}
 
 export default NotFoundPage
